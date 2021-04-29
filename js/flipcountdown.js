62b0200a41a5877980e98cf104947653b34c692f
“123” <123@qq.com>
2021-04-29 13:26:23 +0800

    Site updated: 2021-04-29 13:26:21

--- js/flipcountdown.js
+++ js/flipcountdown.js
@@ -1,1 +1,190 @@
-"use strict";!function(f){jQuery.fn.flipCountDown=jQuery.fn.flipcountdown=function(s){function e(a){function n(t){if(t instanceof Array&&t.length){for(var s,e,a,n=0,o=t.length;n<o;n++)if(c[n]||(c[n]=f('<div class="xdsoft_digit"></div>'),i.before(c[n])),c[n].data("value")!=t[n]){if(e=c[n].data("value"),a=t[n],!e||!a||e!=a&&!/^[0-9]+$/.test(e+""+a)&&!/^[:.\s]+$/.test(e+""+a))switch(c[n].removeClass("xdsoft_separator").removeClass("xdsoft_dot"),t[n]){case":":c[n].addClass("xdsoft_separator");break;case".":c[n].addClass("xdsoft_dot");break;case" ":c[n].addClass("xdsoft_space")}isNaN(t[n])||(s=parseInt(c[n].data("value")),e=parseInt(c[n].data("i")),a=parseInt(t[n]),!isNaN(s)&&n==e||(s=a-1<0?9:a-1),e=c[n],r(e,s=s,(a=a)<s&&(9!=s||0!=a)?-1:1,9!=s||0!=a?Math.abs(s-a):1)),c[n].data("value",t[n]),c[n].data("i",n)}if(c.length>t.length){for(;n<c.length;n++)c[n][0].parentNode.removeChild(c[n][0]),delete c[n];c.splice(t.length)}}}function t(){var t="1",s=[];if(void 0!==(t=d.tick?d.tick instanceof Function?d.tick.call(a,0):d.tick:t)){switch(t.constructor){case Date:var e=(t.getHours()+d.tzoneOffset)%(d.am?12:24);d.showHour&&(s.push(parseInt(e/10)),s.push(e%10)),d.showHour&&(d.showMinute||d.showSecond)&&s.push(":"),d.showMinute&&(s.push(parseInt(t.getMinutes()/10)),s.push(t.getMinutes()%10)),d.showMinute&&d.showSecond&&s.push(":"),d.showSecond&&(s.push(parseInt(t.getSeconds()/10)),s.push(t.getSeconds()%10));break;case String:s=t.replace(/[^0-9\:\.\s]/g,"").split("");break;case Number:s=t.toString().split("")}n(s)}}var s=f('<div class="xdsoft_flipcountdown"></div>'),i=f('<div class="xdsoft_clearex"></div>'),d=f.extend({},u),e=0,r=function t(s,e,a,n){n<1||o(s,-(6*e*p[d.size]+1),1,a,function(){t(s,e+a,a,n-1)},n)},o=function t(s,e,a,n,o,i){e<=60*-p[d.size]&&(e=-1),s.css("background-position","0px "+e+"px"),a<=6?setTimeout(function(){t(s,e-n*p[d.size],++a,n,o,i)},parseInt(d.speedFlip/i)):o()},c=[];s.append(i).on("xdinit.xdsoft",function(){clearInterval(e),d.autoUpdate&&(e=setInterval(t,d.period)),t()}),a.data("setOptions",function(t){d=f.extend({},d,t),p[d.size]||(d.size="lg"),s.addClass("xdsoft_size_"+d.size).trigger("xdinit.xdsoft")}),a.append(s)}var u={showHour:!0,showMinute:!0,showSecond:!0,am:!1,tzoneOffset:0,speedFlip:60,period:1e3,tick:function(){return new Date},autoUpdate:!0,size:"md"},p={lg:77,md:52,sm:35,xs:24};return this.each(function(){var t=f(this);t.data("setOptions")||(t.addClass("xdsoft"),e(t)),t.data("setOptions")&&f.isFunction(t.data("setOptions"))&&t.data("setOptions")(s)})}}(jQuery);
+/** 
+ * @preserve jQuery flipcountdown plugin v3.0.4
+ * @homepage http://xdsoft.net/jqplugins/flipcountdown/
+ * (c) 2013, Chupurnov Valeriy.
+ */
+(function($){
+jQuery.fn.flipCountDown = jQuery.fn.flipcountdown = function( _options ){
+	var default_options = {
+			showHour	:true,
+			showMinute	:true,
+			showSecond	:true,
+			am			:false,
+
+			tzoneOffset	:0,
+			speedFlip	:60,
+			period		:1000,
+			tick		:function(){
+							return new Date();
+						},
+			autoUpdate	:true,
+			size		:'md'
+		},
+		
+		sizes = {
+			lg:77,
+			md:52,
+			sm:35,
+			xs:24
+		},
+		
+		createFlipCountDown = function( $box ){
+			var $flipcountdown 	= $('<div class="xdsoft_flipcountdown"></div>'),
+				$clearex 		= $('<div class="xdsoft_clearex"></div>'),	 
+				
+				options = $.extend({},default_options),
+				
+				timer = 0,
+				
+				_animateRange = function( box,a,b ){
+					_animateOne( box,a,(a>b&&!(a==9&&b==0))?-1:1,!(a==9&&b==0)?Math.abs(a-b):1 );
+				},
+				
+				_animateOne = function( box,a,arrow,range ){
+					if( range<1 )
+						return;
+	
+					_setMargin(box,-(a*6*sizes[options.size]+1),1,arrow,function(){
+						_animateOne(box,a+arrow,arrow,range-1);
+					},range);
+				},
+				
+				_setMargin = function( box, marginTop, rec, arrow,callback,range){
+					if( marginTop<=-sizes[options.size]*60 )
+						marginTop = -1;
+					box.css('background-position','0px '+marginTop+'px' );
+					if( rec<=6 ){
+						setTimeout(function(){
+							_setMargin(box, marginTop-arrow*sizes[options.size], ++rec, arrow, callback,range);	
+						},parseInt(options.speedFlip/range));
+					}else
+						callback();
+				},
+				
+				blocks = [],
+				
+				_typeCompare	= 	function ( a,b ){
+					return 	a&&b&&(
+								(a==b)||
+								(/^[0-9]+$/.test(a+''+b))||
+								(/^[:.\s]+$/.test(a+''+b))
+							);
+				},
+				
+				_generate = function( chars ){
+					if( !(chars instanceof Array) || !chars.length )
+						return false;
+					for( var i = 0, n = chars.length;i<n;i++ ){
+						if( !blocks[i] ){
+							blocks[i] = $('<div class="xdsoft_digit"></div>');
+							$clearex.before(blocks[i]);
+						}
+						if( blocks[i].data('value')!=chars[i] ){
+							if( !_typeCompare(blocks[i].data('value'),chars[i]) ){
+								blocks[i]
+									.removeClass('xdsoft_separator')
+									.removeClass('xdsoft_dot');
+								switch( chars[i] ){
+									case ':':blocks[i].addClass('xdsoft_separator');break;
+									case '.':blocks[i].addClass('xdsoft_dot');break; 
+									case ' ':blocks[i].addClass('xdsoft_space');break; 
+								}
+							}
+							if( !isNaN(chars[i]) ){
+								var old = parseInt(blocks[i].data('value')), 
+									ii = parseInt(blocks[i].data('i')),
+									crnt = parseInt(chars[i]);
+								if( isNaN(old)||i!=ii ){
+									old = (crnt-1)<0?9:crnt-1;
+								}
+								_animateRange(blocks[i],old,crnt);
+							}
+							blocks[i].data('value',chars[i]);
+							blocks[i].data('i',i);
+						}
+					}
+					if( blocks.length>chars.length ){
+						for(;i<blocks.length;i++ ){
+							blocks[i][0].parentNode.removeChild(blocks[i][0]);
+							delete blocks[i];
+						}
+						blocks.splice(chars.length);
+					}
+					
+				},
+				
+				counter = 0,
+				
+				_calcMoment = function(){
+					var value = '1',chars = [];
+					if(options.tick)
+						value = (options.tick instanceof Function)?options.tick.call($box,counter):options.tick;
+					
+					if( typeof value!=='undefined' ){
+						switch( value.constructor ){
+							case Date:
+								var h = (value.getHours()+options.tzoneOffset)%(options.am?12:24);
+		
+								if( options.showHour ){
+									chars.push(parseInt(h/10));
+									chars.push(h%10);
+								}
+										
+								if( options.showHour && (options.showMinute || options.showSecond) )
+									chars.push(':');
+								
+								if( options.showMinute ){
+									chars.push(parseInt(value.getMinutes()/10));
+									chars.push(value.getMinutes() % 10);
+								}
+								
+								if( options.showMinute && options.showSecond )
+									chars.push(':');
+								
+								if( options.showSecond ){
+									chars.push(parseInt(value.getSeconds()/10));
+									chars.push(value.getSeconds() % 10);
+								}
+							break;
+							case String:
+								chars = value.replace(/[^0-9\:\.\s]/g,'').split('');
+							break;
+							case Number:
+								chars = value.toString().split('');
+							break;
+						}
+						_generate(chars);
+					}
+				};
+				
+			$flipcountdown
+				.append($clearex)
+				.on('xdinit.xdsoft',function(){
+					clearInterval(timer);
+					if( options.autoUpdate )
+						timer = setInterval( _calcMoment,options.period );
+					_calcMoment();
+				});
+				
+			$box.data('setOptions',function( _options ){
+				options = $.extend({},options,_options);
+				if( !sizes[options.size] )
+					options.size = 'lg';
+				$flipcountdown
+					.addClass('xdsoft_size_'+options.size)
+					.trigger('xdinit.xdsoft');
+			});
+			$box.append($flipcountdown);
+		};	
+	return this.each(function(){
+		var $box = $(this);
+		if( !$box.data('setOptions') ){
+			$box.addClass('xdsoft')
+			createFlipCountDown($box);
+		}
+		$box.data('setOptions')&&
+			$.isFunction($box.data('setOptions'))&&
+				$box.data('setOptions')(_options);
+	});
+}
+})(jQuery);
--- js/runtime.js
+++ js/runtime.js
@@ -1,1 +1,18 @@
-"use strict";$(function(){var s=Math.round(new Date("4/17/2019 00:00:00").getTime()/1e3);$("#runtime").flipcountdown({size:"xs",tick:function(){function t(t){return 9<t?t:"0"+t}var e=Math.abs(Math.round((new Date).getTime()/1e3)-s),n=86400,r=3600,a=parseInt(e/n),i=parseInt(e%n/r),u=parseInt(e%n%r/60);return t(a)+" "+t(i)+" "+t(u)+" "+t(e%n%r%60)}})});
+$(function(){
+  var NY = Math.round((new Date('4/17/2019 00:00:00')).getTime()/1000);
+  $('#runtime').flipcountdown({
+    size:"xs",
+    tick:function(){
+      var nol = function(h){
+        return h>9?h:'0'+h;
+      }
+      var	range  	= Math.abs(Math.round((new Date()).getTime()/1000)-NY),
+        secday = 86400, sechour = 3600,
+        days 	= parseInt(range/secday),
+        hours	= parseInt((range%secday)/sechour),
+        min		= parseInt(((range%secday)%sechour)/60),
+        sec		= ((range%secday)%sechour)%60;
+      return nol(days)+' '+nol(hours)+' '+nol(min)+' '+nol(sec);
+    }
+  });
+});
